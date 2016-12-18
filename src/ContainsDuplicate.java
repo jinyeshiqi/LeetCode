7fb74dd22cfb022f04074a8d84c7d43f66e02dce
Zichen JIN <jinye.miyu@gmail.com>
2016-12-19 02:40:11 +0900

    217. Contains Duplicate

---
+++ src/ContainsDuplicate.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by jinzichen on 2016/12/19.
+ *
+ * 217. Contains Duplicate
+ * https://leetcode.com/problems/contains-duplicate/
+ */
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
